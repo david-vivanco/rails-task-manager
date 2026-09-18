ee5eb2cf21cb06b70c2e39cc66a7977b9fbf1625
david vivanco <daviddve@hotmail.com>
2026-09-18 16:31:37 +0200

    backend done, frontend to improve, no refacto

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,15 @@ Rails.application.routes.draw do
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
+  get "tasks", to: "tasks#index"
+  get "tasks/new", to: "tasks#new"
+  post "tasks", to: "tasks#create"
+  get "tasks/:id/edit", to: "tasks#edit"
+  patch "tasks/:id", to: "tasks#update"
+  delete "tasks/:id", to: "tasks#destroy"
+  # always last:
+  get "tasks/:id", to: "tasks#show", as: :task
+
   # Defines the root path route ("/")
   # root "posts#index"
 end
